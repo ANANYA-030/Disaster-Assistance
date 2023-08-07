7a123c3acd41d037cc493fddda75d00d22e44fdc
ANANYA-030 <96465658+ANANYA-030@users.noreply.github.com>
2023-08-07 08:56:16 +0530

    Blood donation page .js

--- Blood donation page .js
+++ Blood donation page .js
@@ -1,34 +1,50 @@
 import React, { useEffect, useState } from 'react'
-import { Link } from 'react-router-dom';
 
-const BloodD=()=>{   
+import { Link } from 'react-router-dom';
+const BloodD=()=>{
+
+   
       const[data,setData]= useState([]);
       useEffect(()=>{
         fetch('http://localhost:3001/form1').then(response=> response.json())
         .then(data=>setData(data));
       },[]);
-     return(
+     
+    return(
       <div className='bloodDonationCss'>
         <h1><center>Blood Donation Page</center></h1>
+        <hr/>
         <center>
         <Link to='/Form1'>Blood Request Form</Link>
-        </center> 
+        </center>
+       
         <hr/>
         {data.map((item) => {
           return (<div>
           <table border="1" cellspacing="2" cellpadding="10" width="100%" height="100">
-            <tr> <tr>
-{item && item.gender === 'female' && <img src='images/female.jpeg' width="150"height="200"alt="img"/>}
+         
+            <tr>
+             
+              <td>
+              {item && item.gender === 'female' && <img src='images/female.jpeg' width="150"height="200"alt="img"/>}
               {item && item.gender === 'male' && <img src='images/male.jpeg' width="150"height="200"alt="img"/>}
               </td>
               <th align="left" valign="top"><br/>Patient Name : {item.pName}
                 <br/><br/><br/>Blood Type : {item.bType}<br/><br/><br/>
                 No of units required : {item.units}<br/><br/><br/>
                 Contact Number : {item.cNo}<br/><br/>
-              </th> </tr>        
+              </th>
+            </tr>        
           </table>
-           </div>
-         )})}
-       </div>
-   ); }
+          <br/>
+          </div>
+         )
+        })}
+       
+       
+      </div>
+   );
+  }
+
 export default BloodD
+
